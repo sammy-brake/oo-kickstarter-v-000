e72617889d2fd31ef3a306553352582756b73a92
Samantha brake <sammy.brake@gmail.com>
2018-09-27 02:55:01 +0000

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,14 @@
 class Backer
-  attr_accessor :backed_projects 
+  attr_accessor :backed_projects, :name, :backers, :project  
   
   
-  def initialize
+  def initialize(name)
+    @name = name 
     @backed_projects = []
+  end 
+  
+  def back_project(project)
+    @backed_projects << project 
+   Project.backers.push(self)
   end 
 end 
